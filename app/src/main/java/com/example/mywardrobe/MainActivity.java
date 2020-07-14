1dbc10ba0406ff7571ad7fc8a2ee5b51e29684e9
Jinny Choi <jinchoi99@fb.com>
2020-07-14 02:06:03 -0700

    Create Bottom Nagivation Bar

--- app/src/main/java/com/example/mywardrobe/MainActivity.java
+++ app/src/main/java/com/example/mywardrobe/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.mywardrobe;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG = "MainActivity";
+    final FragmentManager fragmentManager = getSupportFragmentManager();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
