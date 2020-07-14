1dd50702e4fe59da6a3d763f16b8784265d7cbde
Jinny Choi <jinchoi99@fb.com>
2020-07-14 02:30:29 -0700

    Navigate to Fragments from MainActivity

--- app/src/main/java/com/example/mywardrobe/MainActivity.java
+++ app/src/main/java/com/example/mywardrobe/MainActivity.java
@@ -1,17 +1,50 @@
 package com.example.mywardrobe;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.example.mywardrobe.fragments.CalendarFragment;
+import com.example.mywardrobe.fragments.ClothesFragment;
+import com.example.mywardrobe.fragments.OutfitsFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
     public static final String TAG = "MainActivity";
     final FragmentManager fragmentManager = getSupportFragmentManager();
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottomNavigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                Fragment fragment;
+                switch (menuItem.getItemId()) {
+                    case R.id.action_clothes:
+                        fragment = new ClothesFragment();
+                        break;
+                    case R.id.action_outfits:
+                        fragment = new OutfitsFragment();
+                        break;
+                    case R.id.action_calendar:
+                    default:
+                        fragment = new CalendarFragment();
+                        break;
+                }
+                fragmentManager.beginTransaction().replace(R.id.flContainer, fragment).commit();
+                return true;
+            }
+        });
+        // Set default menu item selection so that there is fragment showing, never empty
+        bottomNavigationView.setSelectedItemId(R.id.action_clothes);
     }
 }
