0f1641a313b85c70e1aeb5463a49d47fa92bfd96
Jinny Choi <jinchoi99@fb.com>
2020-07-15 01:32:46 -0700

    Save new Category

---
+++ app/src/main/java/com/example/mywardrobe/activities/ComposeCategoryActivity.java
@@ -0,0 +1,66 @@
+package com.example.mywardrobe.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.example.mywardrobe.R;
+import com.example.mywardrobe.models.Category;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+
+public class ComposeCategoryActivity extends AppCompatActivity {
+
+    public static final String TAG = "ComposeCategoryActivity";
+    private EditText etCategoryName;
+    private Button btnAddCategory;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_compose_category);
+
+        etCategoryName = findViewById(R.id.etCategoryName);
+        btnAddCategory = findViewById(R.id.btnAddCategory);
+
+        //Create a new instance of Category entity/model/parseobject
+        btnAddCategory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String categoryName = etCategoryName.getText().toString();
+                if(categoryName.isEmpty()){
+                    Toast.makeText(ComposeCategoryActivity.this, "Category Name can't be empty", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                ParseUser categoryOwner = ParseUser.getCurrentUser();
+                saveCategory(categoryName, categoryOwner);
+            }
+        });
+    }
+
+    private void saveCategory(String categoryName, ParseUser categoryOwner) {
+        Category category = new Category();
+        category.setCategoryName(categoryName);
+        category.setCategoryOwner(categoryOwner);
+        category.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if(e!=null){
+                    Log.e(TAG, "Error while saving category",e);
+                    Toast.makeText(ComposeCategoryActivity.this, "Error while saving category!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                Log.i(TAG, "Category was saved successfully!");
+                Toast.makeText(ComposeCategoryActivity.this, "Category was saved successfully!", Toast.LENGTH_SHORT).show();
+                etCategoryName.setText("");
+            }
+        });
+    }
+}
--- app/src/main/java/com/example/mywardrobe/models/Category.java
+++ app/src/main/java/com/example/mywardrobe/models/Category.java
@@ -2,16 +2,25 @@ package com.example.mywardrobe.models;
 
 import com.parse.ParseClassName;
 import com.parse.ParseObject;
+import com.parse.ParseUser;
 
 @ParseClassName("Category")
 public class Category extends ParseObject {
     public static final String KEY_CATEGORY_NAME = "categoryName";
     public static final String KEY_CAT_CREATED_KEY = "createdAt";
+    public static final String KEY_CATEGORY_OWNER = "categoryOwner";
 
     public String getCategoryName(){
         return getString(KEY_CATEGORY_NAME);
     }
     public void setCategoryName(String categoryName){
         put(KEY_CATEGORY_NAME, categoryName);
+    }
+
+    public ParseUser getCategoryOwner(){
+        return getParseUser(KEY_CATEGORY_OWNER);
+    }
+    public void setCategoryOwner(ParseUser user){
+        put(KEY_CATEGORY_OWNER, user);
     }
 }
