164ec8ddd06d2e20239e49217fedbd6b075fc668
Jinny Choi <jinchoi99@fb.com>
2020-07-16 16:00:49 -0700

    Capture Image and Save to Parse

--- app/src/main/java/com/example/mywardrobe/activities/ComposeClothingActivity.java
+++ app/src/main/java/com/example/mywardrobe/activities/ComposeClothingActivity.java
@@ -1,9 +1,16 @@
 package com.example.mywardrobe.activities;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -15,10 +22,13 @@ import com.example.mywardrobe.R;
 import com.example.mywardrobe.models.Category;
 import com.example.mywardrobe.models.Clothing;
 import com.parse.ParseException;
+import com.parse.ParseFile;
 import com.parse.ParseUser;
 import com.parse.SaveCallback;
 
 import org.parceler.Parcels;
+
+import java.io.File;
 
 public class ComposeClothingActivity extends AppCompatActivity {
 
@@ -32,6 +42,10 @@ public class ComposeClothingActivity extends AppCompatActivity {
     private Button btnClothingSubmit;
     Category currentCategory;
 
+    public static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 42;
+    private File photoFile;
+    public String photoFileName = "photo.jpg";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -44,6 +58,13 @@ public class ComposeClothingActivity extends AppCompatActivity {
         etPrice = findViewById(R.id.etPrice);
         etBrand = findViewById(R.id.etBrand);
         btnClothingSubmit = findViewById(R.id.btnClothingSubmit);
+
+        btnCaptureImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launchCamera();
+            }
+        });
 
         //Create a new instance of Clothing entity/model/parseobject
         btnClothingSubmit.setOnClickListener(new View.OnClickListener() {
@@ -71,12 +92,70 @@ public class ComposeClothingActivity extends AppCompatActivity {
                 ParseUser clothingOwner = ParseUser.getCurrentUser();
                 currentCategory = Parcels.unwrap(getIntent().getParcelableExtra("categoryName"));
                 final String categoryName = currentCategory.getCategoryName();
-                saveClothing(clothingName, clothingDescription, clothingPrice, clothingBrand, clothingOwner, categoryName);
+//                if(photoFile == null || ivClothing.getDrawable() == null){
+//                    Toast.makeText(ComposeClothingActivity.this, "There is no image!", Toast.LENGTH_SHORT).show();
+//                    return;
+//                }
+                saveClothing(clothingName, clothingDescription, clothingPrice, clothingBrand, clothingOwner, categoryName, photoFile);
             }
         });
     }
 
-    private void saveClothing(String clothingName, String clothingDescription, Number clothingPrice, String clothingBrand, ParseUser clothingOwner, String categoryName) {
+    private void launchCamera() {
+        // create Intent to take a picture and return control to the calling application
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Create a File reference for future access
+        photoFile = getPhotoFileUri(photoFileName);
+
+        // wrap File object into a content provider
+        // required for API >= 24
+        // See https://guides.codepath.com/android/Sharing-Content-with-Intents#sharing-files-with-api-24-or-higher
+        Uri fileProvider = FileProvider.getUriForFile(ComposeClothingActivity.this, "com.codepath.fileprovider.MyWardrobe", photoFile);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider);
+
+        // If you call startActivityForResult() using an intent that no app can handle, your app will crash.
+        // So as long as the result is not null, it's safe to use the intent.
+        if (intent.resolveActivity(getPackageManager()) == null) {
+            // Start the image capture intent to take photo
+            startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                // by this point we have the camera photo on disk
+                Bitmap takenImage = BitmapFactory.decodeFile(photoFile.getAbsolutePath());
+                // RESIZE BITMAP, see section below
+                // Load the taken image into a preview
+                ivClothing.setImageBitmap(takenImage);
+            } else { // Result was a failure
+                Toast.makeText(this, "Picture wasn't taken!", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    // Returns the File for a photo stored on disk given the fileName
+    public File getPhotoFileUri(String fileName) {
+        // Get safe storage directory for photos
+        // Use `getExternalFilesDir` on Context to access package-specific directories.
+        // This way, we don't need to request external read/write runtime permissions.
+        File mediaStorageDir = new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES), TAG);
+
+        // Create the storage directory if it does not exist
+        if (!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()){
+            Log.d(TAG, "failed to create directory");
+        }
+
+        // Return the file target for the photo based on filename
+        File file = new File(mediaStorageDir.getPath() + File.separator + fileName);
+
+        return file;
+    }
+
+    private void saveClothing(String clothingName, String clothingDescription, Number clothingPrice, String clothingBrand, ParseUser clothingOwner, String categoryName, File photoFile) {
         Clothing clothing = new Clothing();
         clothing.setClothingName(clothingName);
         clothing.setClothingDescription(clothingDescription);
@@ -84,6 +163,7 @@ public class ComposeClothingActivity extends AppCompatActivity {
         clothing.setClothingBrand(clothingBrand);
         clothing.setClothingOwner(clothingOwner);
         clothing.setClothingCategory(categoryName);
+        clothing.setClothingImage(new ParseFile(photoFile));
         clothing.saveInBackground(new SaveCallback() {
             @Override
             public void done(ParseException e) {
@@ -98,6 +178,7 @@ public class ComposeClothingActivity extends AppCompatActivity {
                 etClothingDescription.setText("");
                 etPrice.setText("$");
                 etBrand.setText("");
+                ivClothing.setImageResource(0);
             }
         });
         Intent intent = new Intent(this, ClothesActivity.class);
