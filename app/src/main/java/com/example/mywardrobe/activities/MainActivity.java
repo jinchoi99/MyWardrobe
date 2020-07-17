6a1323280a1dfe835107d621121a6317e160c91d
Jinny Choi <jinchoi99@fb.com>
2020-07-17 03:48:04 -0700

    Add NavigationView on Drawer Layout

--- app/src/main/java/com/example/mywardrobe/activities/MainActivity.java
+++ app/src/main/java/com/example/mywardrobe/activities/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.mywardrobe.activities;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
@@ -14,11 +16,15 @@ import com.example.mywardrobe.fragments.CalendarFragment;
 import com.example.mywardrobe.fragments.CategoriesFragment;
 import com.example.mywardrobe.fragments.OutfitsFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
     public static final String TAG = "MainActivity";
     public final FragmentManager fragmentManager = getSupportFragmentManager();
     private BottomNavigationView bottomNavigationView;
+
+    DrawerLayout dlDrawerLayout;
+    NavigationView nvNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,5 +57,11 @@ public class MainActivity extends AppCompatActivity {
         });
         // Set default menu item selection so that there is fragment showing, never empty
         bottomNavigationView.setSelectedItemId(R.id.action_clothes);
+
+        dlDrawerLayout = findViewById(R.id.dlDrawerLayout);
+        nvNavigationView = findViewById(R.id.nvNavigationView);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, dlDrawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        dlDrawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
     }
 }
