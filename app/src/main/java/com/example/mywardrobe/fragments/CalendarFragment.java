4be9cfab7b56f536a33ccf0edc36c3e6e2039a03
Jinny Choi <jinchoi99@fb.com>
2020-07-23 10:20:38 -0700

    Make network call

--- app/src/main/java/com/example/mywardrobe/fragments/CalendarFragment.java
+++ app/src/main/java/com/example/mywardrobe/fragments/CalendarFragment.java
@@ -7,19 +7,30 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CalendarView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.codepath.asynchttpclient.AsyncHttpClient;
+import com.codepath.asynchttpclient.RequestParams;
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
 import com.example.mywardrobe.R;
+
+import okhttp3.Headers;
 
 import static com.parse.Parse.getApplicationContext;
 
 public class CalendarFragment extends Fragment {
+    public static final String TAG = "CalendarFragment";
+    public static final String CURRENT_WEATHER_URL = "https://api.openweathermap.org/data/2.5/weather";
+    //api.openweathermap.org/data/2.5/weather?q={city name}&appid={your api key}
+    private TextView tvWeatherDegree;
 
-    CalendarView cvCalendar;
+    private CalendarView cvCalendar;
 
     public CalendarFragment() {
         // Required empty public constructor
@@ -35,11 +46,29 @@ public class CalendarFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        cvCalendar = (CalendarView) view.findViewById(R.id.cvCalendar); // get the reference of CalendarView
+        cvCalendar = (CalendarView) view.findViewById(R.id.cvCalendar);
         cvCalendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
             public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
                 Toast.makeText(getApplicationContext(), (month+1) + "/" + dayOfMonth + "/" + year, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        tvWeatherDegree = view.findViewById(R.id.tvWeatherDegree);
+
+        AsyncHttpClient client = new AsyncHttpClient();
+        RequestParams params = new RequestParams();
+        params.put("appid", getString(R.string.weather_api_key));
+        params.put("q", "Seoul");
+        client.get(CURRENT_WEATHER_URL, params, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Headers headers, JSON json) {
+                Log.d(TAG, "onSuccess");
+            }
+
+            @Override
+            public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
+                Log.d(TAG, "onFailure");
             }
         });
     }
