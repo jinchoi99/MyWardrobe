99b453e750912233cd7afe10e74a33b64ff5556b
Jinny Choi <jinchoi99@fb.com>
2020-07-23 17:43:02 -0700

    Delete Category

--- app/src/main/java/com/example/mywardrobe/adapters/CategoriesAdapter.java
+++ app/src/main/java/com/example/mywardrobe/adapters/CategoriesAdapter.java
@@ -2,11 +2,13 @@ package com.example.mywardrobe.adapters;
 
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -15,12 +17,19 @@ import com.example.mywardrobe.R;
 import com.example.mywardrobe.activities.ClothesActivity;
 import com.example.mywardrobe.fragments.CategoriesFragment;
 import com.example.mywardrobe.models.Category;
+import com.example.mywardrobe.models.Clothing;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
 
 import org.parceler.Parcels;
 
 import java.util.List;
 
 public class CategoriesAdapter extends RecyclerView.Adapter<CategoriesAdapter.ViewHolder> {
+
+    public static final String TAG = "CategoriesAdapter";
 
     private Context context;
     private List<Category> categories;
@@ -71,12 +80,31 @@ public class CategoriesAdapter extends RecyclerView.Adapter<CategoriesAdapter.Vi
             cbEditCategory = itemView.findViewById(R.id.cbEditCategory);
         }
 
-        public void bind(Category category) {
+        public void bind(final Category category) {
             currentCategory = category;
             btnCategory.setText(category.getCategoryName());
 
             if(CategoriesFragment.deleteCategoryMode){
                 cbDeleteCategory.setVisibility(View.VISIBLE);
+                cbDeleteCategory.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        // Remove from categories list
+                        categories.remove(getAdapterPosition());
+
+                        // Remove category from Parse
+                        try {
+                            category.delete();
+                            Toast.makeText(view.getContext(), "successfully deleted category", Toast.LENGTH_SHORT).show();
+                        } catch (ParseException e) {
+                            e.printStackTrace();
+                        }
+                        // Remove clothes with that category from Parse
+                        removeClothesOfCategory(category.getCategoryName());
+                        CategoriesFragment.deleteCategoryMode=false;
+                        notifyDataSetChanged();
+                    }
+                });
             }
             else {
                 cbDeleteCategory.setVisibility(View.GONE);
@@ -84,10 +112,44 @@ public class CategoriesAdapter extends RecyclerView.Adapter<CategoriesAdapter.Vi
 
             if(CategoriesFragment.editCategoryMode){
                 cbEditCategory.setVisibility(View.VISIBLE);
+                cbEditCategory.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        // TODO:
+                        // Input new name for category
+                        // btnCategory.setText(new name)
+                        // Save changes for the category in Parse
+                        CategoriesFragment.editCategoryMode=false;
+                        notifyDataSetChanged();
+                    }
+                });
             }
             else {
                 cbEditCategory.setVisibility(View.GONE);
             }
+        }
+
+        private void removeClothesOfCategory(String categoryName) {
+            ParseQuery<Clothing> query = ParseQuery.getQuery(Clothing.class);
+            query.whereEqualTo(Clothing.KEY_CLOTHING_OWNER, ParseUser.getCurrentUser());
+            query.whereEqualTo(Clothing.KEY_CLOTHING_CATEGORY, categoryName);
+            query.findInBackground(new FindCallback<Clothing>() {
+                @Override
+                public void done(List<Clothing> clothesOfCategory, ParseException e) {
+                    if(e!=null){
+                        Log.e(TAG, "Issue with getting clothes of the category",e);
+                        return;
+                    }
+                    for(Clothing clothing : clothesOfCategory){
+                        try {
+                            clothing.delete();
+                        } catch (ParseException ex) {
+                            ex.printStackTrace();
+                        }
+                        Log.i(TAG, "Clothing Name: " + clothing.getClothingName());
+                    }
+                }
+            });
         }
     }
 }
