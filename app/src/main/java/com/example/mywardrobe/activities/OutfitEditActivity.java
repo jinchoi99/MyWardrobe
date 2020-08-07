ee29b3c253437694821704a5f5a268bea0387b30
Jinny Choi <jinchoi99@fb.com>
2020-08-07 03:26:47 -0700

    Change colorPrimaryDark Style

--- app/src/main/java/com/example/mywardrobe/activities/OutfitEditActivity.java
+++ app/src/main/java/com/example/mywardrobe/activities/OutfitEditActivity.java
@@ -5,12 +5,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.mywardrobe.R;
+import com.example.mywardrobe.models.Clothing;
+import com.example.mywardrobe.models.Outfit;
+
+import org.parceler.Parcels;
 
 public class OutfitEditActivity extends AppCompatActivity {
+    Outfit currentOutfit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_outfit_edit);
+
+        currentOutfit = Parcels.unwrap(getIntent().getParcelableExtra("currentOutfit"));
+
+
     }
 }
